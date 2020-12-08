7337c22d386fbdff17716a61a80faddf92738f6c
Jari Roossien <jari.roossien@hva.nl>
2020-12-08 12:33:39 +0100

    Added timings
    Signed-off-by: Jari Roossien <jari.roossien@hva.nl>

--- src/nl/jariroossien/aoc/Main.java
+++ src/nl/jariroossien/aoc/Main.java
@@ -9,8 +9,17 @@ public class Main {
         for (int i = 1; i <= 8; i++) {
             Challenge challenge = (Challenge) Class.forName("nl.jariroossien.aoc.days.Day" + i).getDeclaredConstructor().newInstance();
             challenge.setup();
-            System.out.println("Day " + i + " challenge 1: " + challenge.solveOne());
-            System.out.println("Day " + i + " challenge 2: " + challenge.solveTwo());
+            long timer = System.nanoTime();
+            long lOne = challenge.solveOne();
+            long challOneSolveTime = System.nanoTime() - timer;
+
+            System.out.println("Day " + i + " challenge 1: " + lOne + ". solved in " + challOneSolveTime / 1000000 + "ms");
+
+
+            timer = System.nanoTime();
+            long lTwo = challenge.solveTwo();
+            long challengeSolveTime = System.nanoTime() - timer;
+            System.out.println("Day " + i + " challenge 2: " + lTwo + ". solved in " + challengeSolveTime / 1000000 + "ms");
             System.out.println();
         }
 
