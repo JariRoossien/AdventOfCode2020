ecfd0fbbb0de548ff1557b2579e93f05a9514a2f
Jari Roossien <jari.roossien@hva.nl>
2020-12-09 09:15:53 +0100

    Finished Day 9
    Signed-off-by: Jari Roossien <jari.roossien@hva.nl>

--- README.md
+++ README.md
@@ -4,26 +4,29 @@ My solutions to AdventOfCode 2020 edition. [Link to 2020 Advent of Code.](https:
 ## Current Output:
 ```
 Day 1 challenge 1: 787776. solved in 0ms
-Day 1 challenge 2: 262738554. solved in 607ms
+Day 1 challenge 2: 262738554. solved in 647ms
 
-Day 2 challenge 1: 500. solved in 6ms
+Day 2 challenge 1: 500. solved in 4ms
 Day 2 challenge 2: 313. solved in 1ms
 
 Day 3 challenge 1: 276. solved in 0ms
 Day 3 challenge 2: 7812180000. solved in 0ms
 
 Day 4 challenge 1: 282. solved in 0ms
-Day 4 challenge 2: 158. solved in 82ms
+Day 4 challenge 2: 158. solved in 95ms
 
-Day 5 challenge 1: 987. solved in 3ms
+Day 5 challenge 1: 987. solved in 2ms
 Day 5 challenge 2: 603. solved in 0ms
 
 Day 6 challenge 1: 6947. solved in 4ms
-Day 6 challenge 2: 3398. solved in 11ms
+Day 6 challenge 2: 3398. solved in 16ms
 
-Day 7 challenge 1: 172. solved in 70ms
+Day 7 challenge 1: 172. solved in 110ms
 Day 7 challenge 2: 39645. solved in 0ms
 
 Day 8 challenge 1: 1859. solved in 0ms
-Day 8 challenge 2: 1235. solved in 34ms
+Day 8 challenge 2: 1235. solved in 24ms
+
+Day 9 challenge 1: 1309761972. solved in 12ms
+Day 9 challenge 2: 177989832. solved in 39ms
 ```
--- src/nl/jariroossien/aoc/days/Day9.java
+++ src/nl/jariroossien/aoc/days/Day9.java
@@ -6,27 +6,72 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Day9 implements Challenge {
 
+    final static int OFFSET = 25;
+    List<Long> numberSet = new ArrayList<>();
+
     @Override
     public long solveOne() {
+
+        for (int i = OFFSET; i < numberSet.size(); i++) {
+            first:
+            for (int j = i - OFFSET; j < i; j++) {
+                for (int k = i - OFFSET; k < i; k++) {
+
+                    //They are equal, therefor they cant bypass (j = k) but it
+                    // does mean it's at the end of the program.
+                    if (j == i - 1 && k == i - 1) {
+                        return numberSet.get(i);
+                    }
+                    if (j == k) continue;
+
+                    //If there is a number, go to next number in the set.
+                    if (numberSet.get(j) + numberSet.get(k) == numberSet.get(i)) {
+                        break first;
+                    }
+                    //If we are at the last number and there is still no combination, return the number
+                }
+            }
+        }
         return 0;
     }
 
     @Override
     public long solveTwo() {
+        int indexToSolve = numberSet.indexOf(solveOne());
+
+        // Fuck it, just loop through each item until the unsolved index. And increase the size by 1 when unfound.
+        for (int size = 0; size < indexToSolve; size++) {
+            for (int j = 0; j < indexToSolve - size; j++) {
+                List<Long> longList = numberSet.subList(j, j + size);
+
+                //Check if the sum equals to the number set
+                if (longList.stream().mapToLong(Long::longValue).sum() == numberSet.get(indexToSolve)) {
+                    //Sort to not do this 2 times for minimum and maximum
+                    longList.sort(Long::compareTo);
+                    //Make sure it isn't the actual index itself lmao
+                    if (longList.size() >= 2) {
+                        return longList.get(0) + longList.get(longList.size() - 1);
+                    }
+                }
+            }
+        }
         return 0;
     }
 
     @Override
     public void setup() {
-        final File file = new File("input/day8.txt");
+        final File file = new File("input/day9.txt");
         try {
             BufferedReader in = new BufferedReader(new FileReader(file));
             String line;
             while ((line = in.readLine()) != null) {
                 //TODO Put line into a List.
+                numberSet.add(Long.parseLong(line));
             }
         } catch (IOException e) {
             e.printStackTrace();
